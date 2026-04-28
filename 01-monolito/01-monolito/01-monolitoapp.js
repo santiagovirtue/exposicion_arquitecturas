dd10fe18e228279a1d7e151d4d0ab287e5d558ee
Santiago Acevedo <sg5295506@gmail.com>
2026-04-27 19:00:17 -0500

    Set up basic Express server with product routes

--- 01-monolito/01-monolito/01-monolitoapp.js
+++ 01-monolito/01-monolito/01-monolitoapp.js
@@ -1,1 +1,32 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+let productos = [
+  { id: 1, nombre: "Laptop", precio: 2500 },
+  { id: 2, nombre: "Mouse", precio: 80 }
+];
+
+app.get("/", (req, res) => {
+  res.send("Aplicación monolítica funcionando");
+});
+
+app.get("/productos", (req, res) => {
+  res.json(productos);
+});
+
+app.post("/productos", (req, res) => {
+  const nuevoProducto = {
+    id: productos.length + 1,
+    nombre: req.body.nombre,
+    precio: req.body.precio
+  };
+
+  productos.push(nuevoProducto);
+  res.json(nuevoProducto);
+});
+
+app.listen(3000, () => {
+  console.log("Servidor monolítico corriendo en http://localhost:3000");
+});
